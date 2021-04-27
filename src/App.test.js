a009100b612d813b79e80ef119efdd365987a481
Tax <tax.kwon@modulabs.co.kr>
2021-04-27 14:15:02 +0900

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
 
 test('renders learn react link', () => {
   render(<App />);
